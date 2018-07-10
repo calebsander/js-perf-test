f5b8d961e1beeb901a84d8a6836b216b00398877
Caleb Sander <caleb.sander@gmail.com>
2018-07-09 21:26:25 -0700

    Test for copying between ArrayBuffers

---
+++ copy-bytes/index.js
@@ -0,0 +1,26 @@
+const BYTES_TO_COPY = 1 << 20
+const from = new ArrayBuffer(BYTES_TO_COPY)
+new Int8Array(from).fill(-1) // set all bits to 1
+
+module.exports = {
+	by8() {
+		const to = new ArrayBuffer(BYTES_TO_COPY)
+		new Uint8Array(to).set(new Uint8Array(from))
+		return to
+	},
+	by16() {
+		const to = new ArrayBuffer(BYTES_TO_COPY)
+		new Uint16Array(to).set(new Uint16Array(from))
+		return to
+	},
+	by32() {
+		const to = new ArrayBuffer(BYTES_TO_COPY)
+		new Uint32Array(to).set(new Uint32Array(from))
+		return to
+	},
+	by64() {
+		const to = new ArrayBuffer(BYTES_TO_COPY)
+		new BigUint64Array(to).set(new BigUint64Array(from))
+		return to
+	}
+}
